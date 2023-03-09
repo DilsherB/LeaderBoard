52569e1b1f6dc1d8e1912ffc3ee144d288153467
Dilsher Balouch <dilsherb@gmail.com>
2023-03-09 19:23:13 +0300

    2nd milestone achieved.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './style.css';
+import { getData } from './modules/get.js';
+import populate from './modules/populate.js';
+import postData from './modules/post.js';
+
+const dataContainer = document.querySelector('.scoresList');
+const refreshBtn = document.querySelector('.refreshBtn');
+const form = document.querySelector('.form');
+refreshBtn.addEventListener('click', async () => {
+  const { result } = await getData();
+  populate(result, dataContainer);
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const name = e.target.name.value;
+  const score = e.target.score.value;
+  if (!name.trim() || !score) return;
+  postData({
+    user: name,
+    score,
+  });
+  e.target.name.value = '';
+  e.target.score.value = '';
+});
